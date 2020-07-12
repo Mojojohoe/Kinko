7308da783bf9902ea08cec00ff91c6879c924766
Jobix <obixwrepper@gmail.com>
2020-07-12 02:43:35 +0100

    Comparsion popup fix

--- Kinko3_Script.js
+++ Kinko3_Script.js
@@ -743,12 +743,13 @@ comparison();
 } catch (err) {
 
 $("#k_mes").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0});
-$("#k_content").html("This website is sandboxed (That's a good thing!). It means you will need to paste your Kinko code into the box below. If you <i>have</i> a Kinko code that is... If you click the Kinko logo, you will be taken to the tool.<br>There's a 'Copy Kinko Code to Clipboard' button.<br><input type='text' id='c_input' placeholder='Kinko Code Here :)'><br><button onclick='Compare2()' type='button' class='kbut'>Go!</button>")
+$("#k_content").html("This website is sandboxed (That's a good thing!). It means you will need to paste your Kinko code into the box below. If you <i>have</i> a Kinko code that is... If you click the Kinko logo, you will be taken to the tool.<br>There's a 'Copy Kinko Code to Clipboard' button.<br><input type='text' id='c_input' placeholder='Kinko Code Here :)'><br><button onclick='Compare2()' type='button' class='kbut2'>Go!</button>")
 
 }	
 
 }
 function Compare2(){
+	exitC();
 		if ( $( "#kinko-temp" ).length ) {	
 		var k_import = $( "#k_input").val();
     $( "#kinko-temp").html(k_import);
